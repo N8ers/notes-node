33a952df0c32e3a3d86644d00faadf873133b9a6
N8ers <nathanraysheryak@gmail.com>
2018-08-24 10:11:20 -0400

    all done with lecture

--- notes.js
+++ notes.js
@@ -1,19 +1,6 @@
 console.log("Starting notes.js...");
 
-// module.exports.addNote () => {
-//     console.log("addNote");
-//     return 'New note';
-// };
-
-// module.exports.add = (a, b) => {
-//     return a + b;
-// };
-
 module.exports.addNote = () => {
     console.log('addNote');
     return 'New note';
-  };
-  
-  module.exports.add = (a, b) => {
-    return a + b;
   };
