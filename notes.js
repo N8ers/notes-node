c00e8248a5af91c292e9ba2694bb1fd802302b7f
N8ers <nathanraysheryak@gmail.com>
2018-08-24 09:26:57 -0400

    made new function

---
+++ notes.js
@@ -0,0 +1,19 @@
+console.log("Starting notes.js...");
+
+// module.exports.addNote () => {
+//     console.log("addNote");
+//     return 'New note';
+// };
+
+// module.exports.add = (a, b) => {
+//     return a + b;
+// };
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+  };
+  
+  module.exports.add = (a, b) => {
+    return a + b;
+  };
