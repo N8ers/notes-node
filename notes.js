9dc4860b2063c6ac6935be1d826bcc24ae66c675
N8ers <nathanraysheryak@gmail.com>
2018-08-26 08:30:42 -0400

    Yargs and functionality!

--- notes.js
+++ notes.js
@@ -1,6 +1,27 @@
 console.log("Starting notes.js...");
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
-  };
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+//since the name of the object and it's content are 
+//identicle you can state it just once, like above
+
+};
