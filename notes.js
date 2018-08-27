1c904c60c5625846134623ac9e01e07d737e37be
N8ers <nathanraysheryak@gmail.com>
2018-08-27 09:40:43 -0400

    eol

--- notes.js
+++ notes.js
@@ -1,54 +1,50 @@
-console.log("Starting notes.js...");
+console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
-    var note = {
-        title,
-        body
-    };
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
 
-    //wtf is a try catch? it's this...
-    try {
-        var notesString = fs.readFileSync('notes-data.json');
-        //above is pulling old string content
-    
-        notes = JSON.parse(notesString);
-        //this will take the notesstring and parse it into an array
-    } catch(e) {
-
-    }
-    //if try doesn't work it'll go to catch
-
-    var duplicateNotes = notes.filter((note) => note.title === title);
-    //the above is a really clean arrow function!
-
-    if(duplicateNotes.length === 0) {
-        notes.push(note);
-        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-        //two things being passed in; name & content 
-    }
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
 };
 
 var getAll = () => {
-    console.log('Getting all notes');
+  console.log('Getting all notes');
 };
 
 var getNote = (title) => {
-    console.log('Getting note', title);
+  console.log('Getting note', title);
 };
 
 var removeNote = (title) => {
-    console.log('Removing note', title);
+  console.log('Removing note', title);
 };
 
 module.exports = {
-    addNote,
-    getAll,
-    getNote,
-    removeNote
-//since the name of the object and it's content are 
-//identicle you can state it just once, like above
-
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
